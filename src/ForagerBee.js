486e81cfa0869b833e393a1ccee6724f4ed4f9dc
Ivana <ivana.he.lili@gmail.com>
2017-04-12 10:56:03 -0700

    finished

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
+class ForagerBee extends Bee {
+  constructor(color){
+    super(color)
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage (x){
+    this.treasureChest.push(x);
+  }
   // TODO..
 };
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,16 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee {
+  constructor (food, treasureChest){
+    super(food, treasureChest);
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+  }
+  forage (){
+    return 'I am too old, let me play cards instead';
+  }
+  gamble (x){
+    this.treasureChest.push(x);
+  }
   // TODO..
 };
